538494e3881b4c31c90a16ee8de5338351e1124b
atharvak <atharvakadam631@gmail.com>
2025-05-31 11:46:06 +0530

    cors test route 5

--- backend/index.js
+++ backend/index.js
@@ -69,8 +69,8 @@ app.use(express.json());
 const keys = JSON.parse(fs.readFileSync("/etc/secrets/service-account.json", "utf8"));
 
 // Routes
-const authRoutes = require("./routes/authRoutes");
-app.use("/auth", authRoutes);
+// const authRoutes = require("./routes/authRoutes");
+// app.use("/auth", authRoutes);
 
 const testRoutes = require("./routes/testRoutes");
 app.use("/test", testRoutes);
